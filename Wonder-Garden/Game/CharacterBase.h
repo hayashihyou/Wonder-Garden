a31579aa306e4e740fe0b822f0d4226540a942cc
HayashiHyo <CA01244023@st.kawahara.ac.jp>
2025-09-01 17:43:17 +0900

    フォルダ名の変更 (#4)

---
+++ Wonder-Garden/Game/CharacterBase.h
@@ -0,0 +1,52 @@
+#pragma once	
+class CharacterBase : public IGameObject
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	CharacterBase() {};
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~CharacterBase() {};
+
+	/// <summary>
+	/// スタート
+	/// </summary>
+	/// <returns></returns>
+	virtual bool Start();
+
+	/// <summary>
+	/// HPのベース
+	/// </summary>
+	virtual void HP() = 0;
+	
+	/// <summary>
+	/// 攻撃力のベース
+	/// </summary>
+	virtual void Attack() = 0;
+	
+	/// <summary>
+	/// 移動速度のベース
+	/// </summary>
+	virtual void Move() = 0;
+	
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	virtual void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	/// <param name="rc"></param>
+	virtual void Draw(RenderContext& rc);
+
+	int hp;
+	int atk;
+	int spd;
+};
+
---
+++ Wonder-Garden/Game/Player.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "player.h"
+
+bool Player::Start()
+{
+	return false;
+}
+
+void Player::Update()
+{
+	
+}
+
+void Player::Draw(RenderContext& rc)
+{
+
+}
