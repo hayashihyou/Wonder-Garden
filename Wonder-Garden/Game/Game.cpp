0633f8b989c933ca9ea26d46b8f34097fa11d68a
HayashiHyo <CA01244023@st.kawahara.ac.jp>
2025-10-20 17:35:41 +0900

    SkyCubeの更新及びCodeFormatの更新 (#16)

--- Wonder-Garden/Game/Game.cpp
+++ Wonder-Garden/Game/Game.cpp
@@ -1,81 +1,83 @@
 #include "stdafx.h"
-#include "Game.h"
-#include "Player.h"
-#include "PlayerState.h"
+
+#include "Boss.h"
 #include "Enemy.h"
 #include "EnemyType2.h"
-#include "Boss.h"
+#include "EnemyManager.h"
+#include "Game.h"
+#include "GameCamera.h"
+#include "GameClear.h"
+#include "GameOver.h"
+#include "Player.h"
+#include "PlayerState.h"
 #include "Stage.h"
 #include "Star.h"
 #include "StarCounter.h"
 #include "Title.h"
-#include "GameCamera.h"
-#include "GameClear.h"
-#include "GameOver.h"
 
-
-Game::Game()
-{
+Game::Game(){
 
 };
 
 Game::~Game()
 {
-	DeleteGO(m_player);
-	DeleteGO(m_enemy);
-	DeleteGO(m_enemyType2);
-	DeleteGO(m_boss);
-	DeleteGO(m_stage);
-	DeleteGO(m_starCounter);
-	DeleteGO(m_gameCamera);
+    DeleteGO(m_player);
+    EnemyManager::DeleteInstance();
+    DeleteGO(m_boss);
+    DeleteGO(m_stage);
+    DeleteGO(m_skyCube);
+    DeleteGO(m_starCounter);
+    DeleteGO(m_gameCamera);
 }
 
 bool Game::Start()
 {
-	m_player = NewGO<Player>(0, "Player");
-	m_enemy = NewGO<Enemy>(0, "Enemy");
-	m_enemyType2 = NewGO<EnemyType2>(0, "EnemyType2");
-	m_boss = NewGO<Boss>(0, "Boss");
-	m_stage = NewGO<Stage>(0, "Stage");
-	m_starCounter = NewGO<StarCounter>(0, "StarCounter");
-	m_gameCamera = NewGO<GameCamera>(0, "GameCamera");
+    m_player = NewGO<Player>(0, "Player");
+    EnemyManager::CreateInstance();
+    m_boss = NewGO<Boss>(0, "Boss");
+    m_stage = NewGO<Stage>(0, "Stage");
+    m_skyCube = NewGO<SkyCube>(0, "SkyCube");
+    m_starCounter = NewGO<StarCounter>(0, "StarCounter");
+    m_gameCamera = NewGO<GameCamera>(0, "GameCamera");
 
-	//コメントアウトする。
-	//PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
-	return true;
+    // 繧ｳ繝｡繝ｳ繝医い繧ｦ繝医☆繧九�
+    //PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
+
+    EnemyManager::GetInstance()->Setup();
+    return true;
 }
 
 void Game::Update()
 {
-	CreateStar();
+    CreateStar();
 
-	if (m_player->playerPos.y <= -400.0f)
-	{
-		m_gameOver = NewGO<GameOver>(0, "GameOver");
-		DeleteGO(m_star);
-		DeleteGO(this);
-	}
+    EnemyManager::GetInstance()->Update();
 
-	if (m_starCounter->GetStarCount() == 1)
-	{
-		m_gameClear = NewGO<GameClear>(0, "GameClear");
-		DeleteGO(this);
-	}
-		
+    if (m_player->playerPos.y <= -400.0f)
+    {
+        m_gameOver = NewGO<GameOver>(0, "GameOver");
+        DeleteGO(m_star);
+        DeleteGO(this);
+    }
+
+    if (m_starCounter->GetStarCount() == 1)
+    {
+        m_gameClear = NewGO<GameClear>(0, "GameClear");
+        DeleteGO(this);
+    }
 }
 
 void Game::CreateStar()
 {
-	if (m_boss->isDeadFlag == true)
-	{
-		if (m_star == nullptr)
-		{
-			m_star = NewGO<Star>(0, "Star");
-		}
-	}
+    if (m_boss->isDeadFlag == true)
+    {
+        if (m_star == nullptr)
+        {
+            m_star = NewGO<Star>(0, "Star");
+        }
+    }
 }
 
 void Game::Render(RenderContext& rc)
 {
-
 }
