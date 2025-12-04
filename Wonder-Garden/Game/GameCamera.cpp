6dcb4bc851f18f0115e8d44682ffb63536065699
HyouHayashi <CA01244023@st.kawahara.ac.jp>
2025-12-04 13:46:18 +0900

    ファイルのリファクタリング

--- Wonder-Garden/Game/GameCamera.cpp
+++ Wonder-Garden/Game/GameCamera.cpp
@@ -1,65 +1,109 @@
 #include "stdafx.h"
+
 #include "GameCamera.h"
 #include "Player/Player.h"
 
+namespace
+{
+    const float TARGET_HEIGHT = 80.0f;                          // 注視点の高さ
+    const float CAMERA_UPPER_LIMIT = 0.9f;                      // カメラ上方向の制限値
+    const float CAMERA_LOWER_LIMIT = -0.2f;                     // カメラ下方向の制限値
+    const Vector3 INITIAL_TO_CAMERA_POS(0.0f, 125.0f, -250.0f); // 初期注視点からカメラ位置までのベクトル
+    const float CAMERA_NEAR_CLIP = 1.0f;                        // カメラのニアクリップ距離
+    const float CAMERA_FAR_CLIP = 22000.0f;                     // カメラのファークリップ距離
+    const float CAMERA_ROTATION_SPEED = 1.3f;                   // カメラ回転速度
+} // namespace
+
 bool GameCamera::Start()
 {
-	//注視点から視点までのベクトルを設定。
-	m_toCameraPos.Set(0.0f, 125.0f, -250.0f);
+    m_player = FindGO<Player>("Player");
 
-	//Playerのポインタを取得。
-	m_player = FindGO<Player>("Player");
+    // 初期化
+    Init();
 
-	//カメラのニアクリップとファークリップを設定する。
-	g_camera3D->SetNear(1.0f);
-	g_camera3D->SetFar(22000.0f);
-
-	return true;
+    return true;
 }
 
 void GameCamera::Update()
 {
-	//カメラを更新。
-	//注視点を計算する。
-	Vector3 target = m_player->GetPosition();
-	//プレイヤーの足元からちょっと上を注視点とする。
-	target.y += 80.0f;
+    // 入力取得
+    auto inputX = g_pad[0]->GetRStickXF();
+    auto inputY = g_pad[0]->GetRStickYF();
 
-	Vector3 toCameraPosOld = m_toCameraPos;
-	//パッドの入力を使ってカメラを回す。
-	float x = g_pad[0]->GetRStickXF();
-	float y = g_pad[0]->GetRStickYF();
-	//Y軸周りの回転
-	Quaternion qRot;
-	qRot.SetRotationDeg(Vector3::AxisY, 1.3f * x);
-	qRot.Apply(m_toCameraPos);
-	//X軸周りの回転。
-	Vector3 axisX;
-	axisX.Cross(Vector3::AxisY, m_toCameraPos);
-	axisX.Normalize();
-	qRot.SetRotationDeg(axisX, 1.3f * y);
-	qRot.Apply(m_toCameraPos);
-	//カメラの回転の上限をチェックする。
-	//注視点から視点までのベクトルを正規化する。
-	//正規化すると、ベクトルの大きさが１になる。
-	//大きさが１になるということは、ベクトルから強さがなくなり、方向のみの情報となるということ。
-	Vector3 toPosDir = m_toCameraPos;
-	toPosDir.Normalize();
-	if (toPosDir.y < -0.2f) {
-		//カメラが上向きすぎ。
-		m_toCameraPos = toCameraPosOld;
-	}
-	else if (toPosDir.y > 0.9f) {
-		//カメラが下向きすぎ。
-		m_toCameraPos = toCameraPosOld;
-	}
+    // カメラの回転
+    Rotation(inputX, inputY);
 
-	//視点を計算する。
-	Vector3 pos = target + m_toCameraPos;
-	//メインカメラに注視点と視点を設定する。
-	g_camera3D->SetTarget(target);
-	g_camera3D->SetPosition(pos);
+    // カメラの角度抑制
+    SuppressCameraAngle();
 
-	//カメラの更新。
-	g_camera3D->Update();
+    // 注視点計算
+    auto targetPosition = CalcTarget();
+
+    // 視点計算
+    auto cameraPosition = targetPosition + m_toCameraPos;
+
+    // メインカメラに注視点と視点を設定
+    g_camera3D->SetTarget(targetPosition);
+    g_camera3D->SetPosition(cameraPosition);
+
+    // カメラ更新
+    g_camera3D->Update();
+}
+
+void GameCamera::Init()
+{
+    // 注視点から視点までのベクトルを設定
+    m_toCameraPos.Set(INITIAL_TO_CAMERA_POS);
+
+    // カメラのニアクリップとファークリップを設定
+    g_camera3D->SetNear(CAMERA_NEAR_CLIP);
+    g_camera3D->SetFar(CAMERA_FAR_CLIP);
+}
+
+void GameCamera::Rotation(float inputX, float inputY)
+{
+    // Y軸周りの回転
+    Quaternion qRot;
+    qRot.SetRotationDeg(Vector3::AxisY, CAMERA_ROTATION_SPEED * inputX);
+    qRot.Apply(m_toCameraPos);
+
+    // X軸周りの回転
+    Vector3 axisX;
+    axisX.Cross(Vector3::AxisY, m_toCameraPos);
+    axisX.Normalize();
+    qRot.SetRotationDeg(axisX, CAMERA_ROTATION_SPEED * inputY);
+    qRot.Apply(m_toCameraPos);
+}
+
+Vector3 GameCamera::CalcTarget()
+{
+    // プレイヤーの足元から少し上
+    auto target = m_player->GetPosition();
+    target.y += TARGET_HEIGHT;
+
+    return target;
+}
+
+void GameCamera::SuppressCameraAngle()
+{
+    auto toCameraPosOld = m_toCameraPos;
+
+    // 注視点から視点までのベクトル
+    Vector3 toCameraPositionDirection = m_toCameraPos;
+
+    // 正規化
+    // NOTE: 正規化することで、ベクトルの向きだけを取得できる
+    //       大きさが1になるということは、ベクトルの長さ(強さ)情報が失われるため、方向のみを扱うことができる
+    toCameraPositionDirection.Normalize();
+
+    // カメラが上向きすぎ
+    if (toCameraPositionDirection.y < CAMERA_LOWER_LIMIT)
+    {
+        m_toCameraPos = toCameraPosOld;
+    }
+    // カメラが下向きすぎ
+    else if (toCameraPositionDirection.y > CAMERA_UPPER_LIMIT)
+    {
+        m_toCameraPos = toCameraPosOld;
+    }
 }
